69682612b062150d171c2a13536b155ca9abe110
Daniela Tomás <daniela.stomas02@gmail.com>
2023-10-13 18:12:53 +0100

    sort face colors

--- main.py
+++ main.py
@@ -28,26 +28,36 @@ def find_rubiks_cube(frame): # find and highlight the Rubik's Cube pieces
 
     face_colors = []
 
+    #result = np.zeros_like(frame)
+
+    squares_found = 0
+    
     for lower_range, upper_range, color_name in color_ranges:
         mask = cv2.inRange(hsv_frame, np.array(lower_range), np.array(upper_range))
+        
+        #res = cv2.bitwise_and(frame, frame, mask=mask)
+        #cv2.imshow(color_name,res)
+        #result = cv2.add(result, res)
 
         blurred_frame = cv2.GaussianBlur(mask, (5, 5), 0)
 
         kernel = np.ones((5, 5), np.uint8)
-        blurred_frame = cv2.erode(blurred_frame, kernel, iterations=1)
-        blurred_frame = cv2.dilate(blurred_frame, kernel, iterations=1)
+        blurred_frame = cv2.morphologyEx(blurred_frame, cv2.MORPH_OPEN, kernel)
+        blurred_frame = cv2.morphologyEx(blurred_frame, cv2.MORPH_CLOSE, kernel)
 
         edges = cv2.Canny(blurred_frame, 50, 150)
-        
-        contours, _ = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-        squares_found = 0
+        contours, _ = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        #contours, _ = cv2.findContours(edges, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_NONE)
+
+        #cv2.drawContours(frame, contours, -1, (0, 255, 0), 2)
+        
+        #cv2.imshow("Combined Result", result)
 
         for contour in contours:
             area = cv2.contourArea(contour)
-            
             if area > 500 and area < 3000:
-                epsilon = 0.05 * cv2.arcLength(contour, True)
+                epsilon = 0.02 * cv2.arcLength(contour, True)
                 approx = cv2.approxPolyDP(contour, epsilon, True)
                 
                 if is_square(approx):
@@ -56,11 +66,18 @@ def find_rubiks_cube(frame): # find and highlight the Rubik's Cube pieces
                     x, y, w, h = cv2.boundingRect(contour)
                     cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
                     cv2.putText(frame, color_name, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
-                    face_colors.append(color_name) #TODO guardar também coodenadas e ordena-las depois
+                    face_colors.append((color_name, (x, y)))
 
-        if squares_found == 9: #TODO and set(face) == set(face_colors)
-            face.append(list(face_colors))
-            print("detected all the pieces of this face")
+    if squares_found == 9:
+        face_colors.sort(key=lambda x: x[1][1]) # sort by y
+        groups = [face_colors[i:i + 3] for i in range(0, len(face_colors), 3)] # split into groups of three
+        # sort each group by x
+        for group in groups:
+            group.sort(key=lambda x: x[1][0])
+
+        if groups not in face: #TODO
+            face.append(groups)
+            print(f"{groups[1][1][0]} face detected!")
             
     return frame
 
@@ -90,8 +107,12 @@ def main():
     cv2.destroyAllWindows()
 
     # print
-    for i, colors in enumerate(face):
-        print(f"Colors in face {i + 1}: {', '.join(colors)}")
+    for groups in face:
+        print(f"{groups[1][1][0]} face")
+        for group in groups:
+            for color, coordinates in group:
+                print(f"  Color: {color}, Coordinates: {coordinates}")
+    
 
 if __name__ == "__main__":
     main()
